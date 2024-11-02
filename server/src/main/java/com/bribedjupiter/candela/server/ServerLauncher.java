2d4798c9477164e91c58984d8785ef61cb3a3c68
Jack <jgb1017@gmail.com>
2024-11-02 12:45:25 -0500

    Initial commit

---
+++ server/src/main/java/com/bribedjupiter/candela/server/ServerLauncher.java
@@ -0,0 +1,8 @@
+package com.bribedjupiter.candela.server;
+
+/** Launches the server application. */
+public class ServerLauncher {
+    public static void main(String[] args) {
+        // TODO Implement server application.
+    }
+}
